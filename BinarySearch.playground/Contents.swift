90c66fd107f36b3bb7e029b899878bbbabb482e3
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-19 18:33:05 -0700

    Completes recursive search function with generics

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -23,6 +23,26 @@ public func dynamicSearch<T: Comparable >(array: [T], search: T) -> Bool? {
     return nil
 }
 
+public func recursiveSearch<T: Comparable >(array: [T], search: T, range: Range<Int>) -> Bool? {
+    
+    let midIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
+    
+    guard range.lowerBound != range.upperBound else {
+        return false
+    }
+    
+    if array[midIndex] == search {
+        return true
+    } else if array[midIndex] > search {
+        return recursiveSearch(array: array, search: search, range: range.lowerBound..<midIndex - 1)
+    } else if array[midIndex] < search {
+        return recursiveSearch(array: array, search: search, range: midIndex + 1..<range.upperBound)
+    }
+    
+    return false
+}
+
 let arr = [1,2,3,4,5]
 
+recursiveSearch(array: arr, search: 3, range: 0..<arr.count)
 dynamicSearch(array: arr, search: 4)
