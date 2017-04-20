ed0c2a09b17235507ed959ef14429a112c632b9a
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-20 13:23:06 -0700

    Completed with LogN recursive solution

--- TreeSearch.playground/Contents.swift
+++ TreeSearch.playground/Contents.swift
@@ -9,26 +9,48 @@
  */
 
 class Node {
-    var searchVal: Int
+    var value: Int
     var leftBranch: Node?
     var rightBranch: Node?
     
-    init(left: Node, right: Node, search: Int) {
+    init(left: Node?, right: Node?, value: Int) {
         leftBranch = left
         rightBranch = right
-        searchVal = search
+        self.value = value
     }
 }
+
+let eightNode = Node(left: nil, right: nil, value: 8)
+let twelveNode = Node(left: nil, right: nil, value: 12)
+
+//Right nodes
+let thirtyNode = Node(left: eightNode, right: twelveNode, value: 30)
+let sevenNode = Node(left: nil, right: nil, value: 7)
+
+//Left nodes
+let fiveNode = Node(left: nil, right: nil, value: 5)
+let twentyNode = Node(left: fiveNode, right: sevenNode, value: 20)
+
+//First node
+let tenNode = Node(left: twentyNode, right: thirtyNode, value: 10)
 
 func searchTree(node: Node?, searchVal: Int) -> Bool {
     
-    guard node!.searchVal != searchVal else {
+    guard node != nil else {
+        return false
+    }
+    
+    if node?.value == searchVal {
         return true
     }
-
-    return searchTree(node: node!.leftBranch, searchVal: searchVal) || searchTree(node: node!.rightBranch, searchVal: searchVal)
     
-    return false
+//LogN solution. Improvement above.
+//    } else {
+//        return searchTree(node: node?.leftBranch, searchVal: searchVal) || searchTree(node: node?.rightBranch, searchVal: searchVal)
+//    }
+    
 }
+
+searchTree(node: tenNode, searchVal: 12)
 
 
