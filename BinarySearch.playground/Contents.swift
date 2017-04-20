a22847dd730e5dca6376c279d5fd08e5da44864c
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-19 18:56:31 -0700

    Simplifies dynamic search

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -10,18 +10,33 @@ public func dynamicSearch<T: Comparable >(array: [T], search: T) -> Bool? {
         
         let midIndex = bottomIndex + (topIndex - bottomIndex) / 2
         
-        // Set a base case so that when the centermost value == searched, return true.
+        // Set a base case so once the centermost value == searched, return true.
         if array[midIndex] == search {
             return true
         } else if array[midIndex] > search {
             topIndex = midIndex - 1
         } else if array[midIndex] < search {
-            bottomIndex = midIndex - 1
+            bottomIndex = midIndex + 1
+        } else {
+            return false
         }
     }
     
-    return nil
+    return false
 }
+
+public func refactoredDynamicSearch <T: Comparable>(array:[T] , search: T) -> Bool? {
+    
+    for i in 0..<array.count {
+        if array[i] == search {
+            return true
+        }
+    }
+    
+    return false
+}
+
+// MARK: - Recursive solution. TODO:
 
 public func recursiveSearch<T: Comparable >(array: [T], search: T, range: Range<Int>) -> Bool? {
     
@@ -31,18 +46,24 @@ public func recursiveSearch<T: Comparable >(array: [T], search: T, range: Range<
         return false
     }
     
-    if array[midIndex] == search {
+    if array[midIndex] == search { // Set a base case so once the centermost value == searched, return true.
         return true
     } else if array[midIndex] > search {
-        return recursiveSearch(array: array, search: search, range: range.lowerBound..<midIndex - 1)
+        return recursiveSearch(array: array,
+                               search: search,
+                               range: range.lowerBound..<midIndex - 1) // Decrement from the top (middle)
     } else if array[midIndex] < search {
-        return recursiveSearch(array: array, search: search, range: midIndex + 1..<range.upperBound)
+        return recursiveSearch(array: array,
+                               search: search,
+                               range: midIndex + 1..<range.upperBound) //Increment form the bottom (middle)
     }
     
     return false
 }
 
-let arr = [1,2,3,4,5]
+let intArr = [1, 2, 3, 4, 5]
+let arr = ["cat", "dog", "squirrel", "person"]
 
-recursiveSearch(array: arr, search: 3, range: 0..<arr.count)
-dynamicSearch(array: arr, search: 4)
+recursiveSearch(array: arr, search: "person", range: 0..<arr.count)
+dynamicSearch(array: intArr, search: 1)
+refactoredDynamicSearch(array: intArr, search: 5)
