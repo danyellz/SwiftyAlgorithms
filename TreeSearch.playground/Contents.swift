40cca86854cf0477a717860ebf666c1bf3499817
Tieshow Daniels <tieshowdaniels@Tieshows-MacBook-Pro.local>
2017-04-30 16:08:05 -0700

    Updates tree search with Generics

--- TreeSearch.playground/Contents.swift
+++ TreeSearch.playground/Contents.swift
@@ -7,68 +7,61 @@
   5 20 6 31
 */
 
-
-// MARK: - Objects
-
-class Node {
-    var value: Int
-    var leftBranch: Node?
-    var rightBranch: Node?
+class Node<T> {
     
-    init(left: Node?, right: Node?, value: Int) {
+    var value: T
+    var leftBranch: Node<T>?
+    var rightBranch: Node<T>?
+    
+    init(left: Node<T>?, right: Node<T>?, value: T) {
         leftBranch = left
         rightBranch = right
         self.value = value
     }
 }
 
-//Right nodes
-let sixNode = Node(left: nil, right: nil, value: 6)
-let thirtyOneNode = Node(left: nil, right: nil, value: 31)
-let thirtyNode = Node(left: sixNode, right: thirtyOneNode, value: 30)
+class Tree<T> {
 
-//Left nodes
-let fiveNode = Node(left: nil, right: nil, value: 5)
-let twentyNode = Node(left: nil, right: nil, value: 20)
-let eightNode = Node(left: fiveNode, right: twentyNode, value: 8)
-
-//First node
-let tenNode = Node(left: eightNode, right: thirtyNode, value: 10)
-
-func searchTree(node: Node?, searchVal: Int) -> Bool {
-    
-    print(node?.value, searchVal)
-    
-    guard node != nil else {
-        return false
+    func treeSort<T: Equatable>(node: Node<T>?, searchVal: T) -> Bool {
+        
+        guard node != nil else {
+            return false
+        }
+        
+        if node?.value == searchVal {
+            return true
+        } else {
+            return treeSort(node: node?.leftBranch!, searchVal: searchVal) || treeSort(node: node?.rightBranch!, searchVal: searchVal)
+        }
+        /*
+         NOTE: - This solution assumes that all left nodes are decreasing from their parent,
+         while right nodes are increasing in value.
+         */
+        
+        //    else if (searchVal < node!.value) {
+        //        return searchTree(node: node?.leftBranch, searchVal: searchVal)
+        //    } else {
+        //        return searchTree(node: node?.rightBranch, searchVal: searchVal)
+        //    }
     }
-    
-    if node?.value == searchVal {
-        return true
-    }
-    
-    /*
-     NOTE: - LogN solution. Indifferent to increasing/decreasing values.
-     Speed improvement above.
-     */
-    else {
-        return searchTree(node: node?.leftBranch, searchVal: searchVal) || searchTree(node: node?.rightBranch, searchVal: searchVal)
-    }
-    
-    /*
-     NOTE: - This solution assumes that all left nodes are decreasing from their parent,
-     while right nodes are increasing in value.
-     */
-    
-    //    else if (searchVal < node!.value) {
-    //        return searchTree(node: node?.leftBranch, searchVal: searchVal)
-    //    } else {
-    //        return searchTree(node: node?.rightBranch, searchVal: searchVal)
-    //    }
-    
-    return false
 }
 
-searchTree(node: tenNode, searchVal: 20)
+
+//Right nodes
+let sixNode: Node<Int> = Node<Int>(left: nil, right: nil, value: 6)
+let thirtyOneNode = Node<Int>(left: nil, right: nil, value: 31)
+let thirtyNode = Node<Int>(left: sixNode, right: thirtyOneNode, value: 30)
+
+//Left nodes
+let fiveNode = Node<Int>(left: nil, right: nil, value: 5)
+let twentyNode = Node<Int>(left: nil, right: nil, value: 20)
+let eightNode = Node<Int>(left: fiveNode, right: twentyNode, value: 8)
+
+//First node
+let tenNode = Node<Int>(left: eightNode, right: thirtyNode, value: 10)
+
+
+let searchTree = Tree<Int>()
+searchTree.treeSort(node: tenNode, searchVal: 20)
 
 
